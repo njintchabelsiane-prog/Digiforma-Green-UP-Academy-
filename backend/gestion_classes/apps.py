cda9c93b70254342d0106d93c838d35cab7503a3
dosy237 <dosyca35@gmail.com>
2026-05-28 15:30:53 +0200

    feat(classes): ajouter app gestion_classes et modele Classe

---
+++ backend/gestion_classes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionClassesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestion_classes'
--- backend/greenup_backend/settings.py
+++ backend/greenup_backend/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'corsheaders',
     # Nos apps
     'users',
+    'gestion_classes',
 ]
 
 MIDDLEWARE = [
