db92e1b0b1d6c4d819cb73a6cf14f614da455047
priscaprilaine@gmail.com <priscaprilaine@gmail.com>
2026-06-11 11:24:21 +0100

    feat: add ClassePage css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import ModalArchiver from "./ModalArchiver ";
+import PageClasse from "./PageClasse";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/rejoindre" element={<ModalArchiver />} />
+        <Route path="/rejoindre" element={<PageClasse />} />
         <Route path="*" element={<Navigate to="/rejoindre" replace />} />
       </Routes>
     </Router>
